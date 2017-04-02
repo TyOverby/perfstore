c3ceb228cd9bfb4b6ff1a7174c338d2c3cae6a0c
Ty Overby <ty@pre-alpha.com>
2017-04-02 00:29:35 +0000

    add web server component

---
+++ server/Webbernet.fs
@@ -0,0 +1,32 @@
+﻿namespace PerfStore
+
+open Suave
+open Option
+open Suave.Successful
+open Suave.Web
+open Suave.Filters
+open Suave.Operators
+open Suave.Writers
+open FSharp.Data
+open Suave.Files
+open Suave.RequestErrors
+open Suave.Logging
+open Suave.Utils
+
+module Webbernet = 
+    open System
+
+    let staticFiles =
+        let dir = match Seq.tryItem 1 (Environment.GetCommandLineArgs()) with
+                    | Some(x) -> x 
+                    | None -> Environment.CurrentDirectory
+        dir |> IO.Path.GetFullPath
+
+    let app = choose [
+            //path "/websocket" >=> handShake echo
+        GET >=> choose [
+            Files.browse staticFiles
+        ]
+    ]
+
+    let start = fun() -> startWebServer defaultConfig app
